abf1a0d77a2de20330c121bbabaaafc0a2205dbd
Sahil Dahal <denenme49@gmail.com>
2025-09-04 14:33:24 +0545

    Refactor registration schema to improve password validation regex and formatting

--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -13,11 +13,14 @@ export const loginSchema = z.object({
 //  registerSchema is used for validating user registration input
 export const registerSchema = loginSchema.extend({
     name: z.string().min(1, "Name is required"),
-    confirmPassword: z.string().min(6, "Confirm Password must be at least 6 characters"),
-    password: z.string()
+    confirmPassword: z
+        .string()
+        .min(6, "Confirm Password must be at least 6 characters"),
+    password: z
+        .string()
         .min(6, "Password must be at least 6 characters")
         .regex(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).*$/,
             "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
         ),
 }).refine((data) => data.password === data.confirmPassword, {
