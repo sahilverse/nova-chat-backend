3376909f7a05b50fb5e9a8b6a8c660a5c91c152f
Sahil Dahal <denenme49@gmail.com>
2025-09-08 11:17:19 +0545

    Fix: Update Redis client URL to enforce environment variable usage

--- src/config/redisClient.ts
+++ src/config/redisClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-    url: process.env.REDIS_URL || "redis://localhost:6379",
+    url: process.env.REDIS_URL!,
 });
 
 client.connect().then(() => {
