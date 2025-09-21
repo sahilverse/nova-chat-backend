87557c5a26297f61ec762cdfaf10394dd898470c
Sahil Dahal <denenme49@gmail.com>
2025-09-21 15:46:15 +0545

    Add: Validation Middleware

---
+++ src/middlewares/validation.middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodObject, ZodSafeParseResult } from "zod";
+import { ResponseHandler } from "../utils";
+
+
+
+export const validateRequest = (schema: ZodObject<any>) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const result: ZodSafeParseResult<any> = schema.safeParse(req.body);
+        if (!result.success) {
+            ResponseHandler.sendValidationError(res, result);
+            return;
+        }
+        req.body = result.data;
+        next();
+    };
+}
