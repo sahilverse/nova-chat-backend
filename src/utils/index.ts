44c2a6be3ff0e85b052b31b9a67c63ca7ab4a7b1
Sahil Dahal <denenme49@gmail.com>
2025-09-22 11:22:15 +0545

    Add export for cloudinary service in utils index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export { default as BcryptUtils } from "./bcrypt"
 export { default as JwtUtils } from "./jwt"
 export { default as ResponseHandler } from "./responseHandler"
+export * from "./cloudinary.service"
 export * from "./zod"
 
